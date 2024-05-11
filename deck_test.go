5d4205841a17eadb334e303e533fdbe0b7507495
Koraxtu <bukason2013@gmail.com>
2024-05-11 03:00:15 -0400

    Final version

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 // t.Errorf("Expected deck length of 16, but got %v", len(d))
 
@@ -10,4 +13,19 @@ func TestNewDeck(t *testing.T) {
 	if len(testCards) != 28 {
 		t.Errorf("Expected deck length of 28, but got %v", len(testCards))
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 28 {
+		t.Errorf("Expected deck length of 28, but got %v", len(loadedDeck))
+
+	}
+	os.Remove("_decktesting")
 }
